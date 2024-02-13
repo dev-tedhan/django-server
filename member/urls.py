4421bdca970798d81bcf847972aaf4ecfd41e98b
dev.tedhan <dev.tedhan@gmail.com>
2024-02-13 10:06:50 +0900

    removed jwt

--- member/urls.py
+++ member/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
-from django.views.generic import TemplateView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 from member.views import MemberJoinView, MemberLoginView, MemberCheckId, MyPageView
 
