887a4cc4900ede55cb5aa56a868fd03b62268e6a
dev.tedhan <dev.tedhan@gmail.com>
2024-02-13 10:09:03 +0900

    default value of updated date changed to timezone.now

--- project/models.py
+++ project/models.py
@@ -1,11 +1,12 @@
 import datetime
 
 from django.db import models
+from django.utils import timezone
 
 
 class Period(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
-    updated_date = models.DateTimeField(default=datetime.datetime.now())
+    updated_date = models.DateTimeField(default=timezone.now)
 
     class Meta:
         abstract = True
