067292f07418382f41a0a391442df4983db55e80
Ubuntu <ubuntu@ip-172-31-7-87.ap-northeast-2.compute.internal>
2024-02-13 11:13:01 +0900

    aborted

--- staticfiles/reply/js/reply.js
+++
@@ -1,6 +0,0 @@
-const writeButton = document.getElementById("reply-write");
-
-writeButton.addEventListener("click", async (e) => {
-    const replyContent = document.getElementById("reply-content");
-    await replyService.write(postId, replyContent.value);
-})
